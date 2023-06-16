ae19486b3904c4e37f76470e2dec872bcd6781b5
TAMIZH7 <109167888+TAMIZH7@users.noreply.github.com>
2023-06-16 14:03:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const quotes=[{
     person:'Soren Kierkegaard'
 },
 {
-    quote:"Everybody wants to be famous, but nobody wants to do the work. I live by that. You grind hard so you can play hard. At the end of the day, you put all the work in, and eventually it’ll pay off. It could be in a year, it could be in 30 years. Eventually, your hard work will pay off.",
+    quote:"Everybody wants to be famous, but nobody wants to do the work. I live by that. You grind hard so you can play hard. Eventually, your hard work will pay off.",
     perosn:'Kevin cart'
 },
 {
